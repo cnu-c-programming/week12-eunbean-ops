e7913a2c3f7c00b48578cc210dff26db8e565f32
eunbean-ops <rida1223@naver.com>
2026-05-26 14:47:30 +0900

    2-2.c
    Refactor student score calculation and improve variable naming.

--- code/2-2.c
+++ code/2-2.c
@@ -10,34 +10,32 @@ int main(int argc, const char* argv[]) {
     FILE* fp = fopen("student.txt", "r");
     int count = 0;
     Student students[64];
-    
-   while (!feof(fp)) {
-      
-        if (count >= 64) break; 
+
+    while (!feof(fp)) {
         if (fscanf(fp, "%s %d", students[count].name, &students[count].score) == 2) {
-     
+            count++;
         }
     }
 
     int max = 0;
     float avg = 0;
-  if (count > 0) { 
-        int sum = 0;
-        max = students[0].score; 
-        
+
+    if (count > 0) {
+        int total_sum = 0;
+        max = students[0].score;
+
         for (int i = 0; i < count; i++) {
-            sum += students[i].score;
+            total_sum += students[i].score;
             if (students[i].score > max) {
                 max = students[i].score;
             }
         }
-        avg = (float)sum / count;
-
+        avg = (float)total_sum / count;
+    }
 
     printf("max: %d\n", max);
     printf("avg: %.2f\n", avg);
 
     fclose(fp);
-
     return 0;
-}}
+}
