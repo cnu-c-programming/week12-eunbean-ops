7fa37d14561a1005eb0b6c50a6d7a878e01ad719
eunbean-ops <rida1223@naver.com>
2026-05-26 14:34:21 +0900

    3-5.c
    Added error handling for invalid input values while reading integers from a file.

--- code/3-5.c
+++ code/3-5.c
@@ -8,9 +8,23 @@ int main(int argc, const char* argv[]) {
         return 0;
     }
 
-
     int sum = 0;
+    int num;
+    char invalid_str[256];
 
+    while (1) {
+        int result = fscanf(fp, "%d", &num);
+        if (result == EOF) {
+            break;
+        }
+
+        if (result == 1) {
+            sum += num;
+        }else {
+            fscanf(fp, "%255s", invalid_str);
+            fprintf(stderr, "error: invalid value '%s'\n", invalid_str);
+        }
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
