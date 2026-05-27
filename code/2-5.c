5a75ea136b060c122c6be50fdcb70d5b2b7fa779
eunbean-ops <rida1223@naver.com>
2026-05-27 15:45:27 +0900

    2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -6,7 +6,7 @@ int main(int argc, const char* argv[]) {
     FILE* fp_src= fopen(argv[1],"r" );
     FILE* fp_dst= fopen(argv[2], "w");
     int ch;
-    while((ch==fgetc(fp_src))!= EOF){
+    while((ch=fgetc(fp_src))!= EOF){
         fputc(ch, fp_dst);
     }
 
