08fd46f79853b1d7b8d243c95988dd028d2493c1
eunbean-ops <rida1223@naver.com>
2026-05-27 16:01:47 +0900

    3-5.c
    Reduced the size of the invalid_str array and improved error handling for invalid input.

--- code/3-5.c
+++ code/3-5.c
@@ -10,19 +10,20 @@ int main(int argc, const char* argv[]) {
 
     int sum = 0;
     int num;
-    char invalid_str[256];
+    char invalid_str[64];
 
     while (1) {
         int result = fscanf(fp, "%d", &num);
-        if (result == EOF) {
-            break;
-        }
-
+        
         if (result == 1) {
             sum += num;
-        }else {
-            fscanf(fp, "%255s", invalid_str);
-            fprintf(stderr, "error: invalid value '%s'\n", invalid_str);
+        } 
+        else if (result == 0) {
+            fscanf(fp, "%63s", invalid_str);
+            fprintf(stderr, "invalid input %s\n", invalid_str);
+        } 
+        else {
+            break;
         }
     }
 
